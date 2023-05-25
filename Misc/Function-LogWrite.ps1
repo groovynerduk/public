124311dafdd1a3eec48c0ef9018b8e3375b34a90
ChrisRobb93 <41024073+ChrisRobb93@users.noreply.github.com>
2023-05-25 17:37:45 +0100

    Create Function-LogWrite.ps1

---
+++ Misc/Function-LogWrite.ps1
@@ -0,0 +1,37 @@
+function Write-log {
+
+    [CmdletBinding()]
+    Param(
+          [parameter(Mandatory=$true)]
+          [String]$Path,
+
+          [parameter(Mandatory=$true)]
+          [String]$Message,
+
+          [parameter(Mandatory=$true)]
+          [String]$Component,
+
+          [Parameter(Mandatory=$true)]
+          [ValidateSet("Info", "Warning", "Error")]
+          [String]$Type
+    )
+
+    switch ($Type) {
+        "Info" { [int]$Type = 1 }
+        "Warning" { [int]$Type = 2 }
+        "Error" { [int]$Type = 3 }
+    }
+
+    # Create a log entry
+    $Content = "<![LOG[$Message]LOG]!>" +`
+        "<time=`"$(Get-Date -Format "HH:mm:ss.ffffff")`" " +`
+        "date=`"$(Get-Date -Format "M-d-yyyy")`" " +`
+        "component=`"$Component`" " +`
+        "context=`"$([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)`" " +`
+        "type=`"$Type`" " +`
+        "thread=`"$([Threading.Thread]::CurrentThread.ManagedThreadId)`" " +`
+        "file=`"`">"
+
+    # Write the line to the log file
+    Add-Content -Path $Path -Value $Content
+}
